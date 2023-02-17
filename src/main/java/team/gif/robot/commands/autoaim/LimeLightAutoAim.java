9138bffabdbbc0c0190fa65ed3f9a6a21b625b53
KenV <30897541@students.sd308.org>
2023-02-16 18:07:45 -0600

    limelight auto allign

---
+++ src/main/java/team/gif/robot/commands/autoaim/LimeLightAutoAim.java
@@ -0,0 +1,54 @@
+package team.gif.robot.commands.autoaim;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import team.gif.robot.Constants;
+import team.gif.robot.Robot;
+import team.gif.robot.subsystems.drivers.Limelight;
+
+
+public class LimeLightAutoAim extends CommandBase {
+
+    private final double yTolerence = 2.0;
+    private double yOffset;
+    public LimeLightAutoAim() {}
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        Robot.limelight.setLEDMode(Limelight.LED_ON); // turn on - just in case they were turned off somehow
+    }
+
+    // Called every time the scheduler runs (~20ms) while the command is scheduled
+    @Override
+    public void execute() {
+
+
+        if (Robot.limelight.hasTarget()) {
+
+            yOffset = -Robot.limelight.getXOffset();
+
+            ChassisSpeeds chassisSpeeds = new ChassisSpeeds(0, .5, 0.0);
+            SwerveModuleState[] moduleStates = Constants.Drivetrain.DRIVE_KINEMATICS.toSwerveModuleStates(chassisSpeeds);
+            Robot.swervetrain.setModuleStates(moduleStates);
+        }
+    }
+
+    // Return true when the command should end, false if it should continue. Runs every ~20ms.
+    @Override
+    public boolean isFinished() {
+        if (Math.abs(yOffset) < yTolerence)
+            return true;
+        else
+            return false;
+    }
+
+    // Called when the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        ChassisSpeeds chassisSpeeds = new ChassisSpeeds(0, 0, 0.0);
+        SwerveModuleState[] moduleStates = Constants.Drivetrain.DRIVE_KINEMATICS.toSwerveModuleStates(chassisSpeeds);
+        Robot.swervetrain.setModuleStates(moduleStates);
+    }
+}
