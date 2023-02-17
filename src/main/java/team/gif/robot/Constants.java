9cd596b933843adb4fcc651835cb12c6ad2d009a
Tharun S <30896231@students.sd308.org>
2023-02-17 09:49:10 -0600

    code clean-up

--- src/main/java/team/gif/robot/Constants.java
+++ src/main/java/team/gif/robot/Constants.java
@@ -169,7 +169,7 @@ public final class Constants {
     public static class TelescopingArm {
         public static final double TEST = 0.2;
         public static final double MAX_VELOCITY = 0.4; // TODO: need more testing
-        public static final double MIN_VELOCITY =
+        public static final double MIN_VELOCITY = 0; // TODO: need more testing
     }
 
     public static class Collector {
