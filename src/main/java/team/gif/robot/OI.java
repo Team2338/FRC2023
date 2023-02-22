6e081b101f5b3e3e3a9b8d3347d6c7ce06f200dd
Lovuntaes <16002187@students.sd308.org>
2023-02-21 20:52:16 -0600

    troubleshooting

--- src/main/java/team/gif/robot/OI.java
+++ src/main/java/team/gif/robot/OI.java
@@ -1,10 +1,14 @@
 package team.gif.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import team.gif.lib.AxisButton;
 import team.gif.robot.commands.arm.MoveUp;
 import team.gif.robot.commands.collector.CollectorEject;
@@ -25,27 +29,29 @@ public class OI {
      * private final JoystickButton leftTrigger = new JoystickButton(leftStick, 0);
      */
 
-    public final XboxController driver = new XboxController(RobotMap.DRIVER_CONTROLLER_ID);
+    public final CommandXboxController driver = new CommandXboxController(RobotMap.DRIVER_CONTROLLER_ID);
     public final XboxController aux = new XboxController(RobotMap.AUX_CONTROLLER_ID);
     public final XboxController test = new XboxController(RobotMap.TEST_CONTROLLER_ID);
 
-    public final JoystickButton dA = new JoystickButton(driver, 1);
-    public final JoystickButton dB = new JoystickButton(driver, 2);
-    public final JoystickButton dX = new JoystickButton(driver, 3);
-    public final JoystickButton dY = new JoystickButton(driver, 4);
-    public final JoystickButton dLBump = new JoystickButton(driver, 5);
-    public final JoystickButton dRBump = new JoystickButton(driver, 6);
-    public final JoystickButton dBack = new JoystickButton(driver, 7);
-    public final JoystickButton dStart = new JoystickButton(driver, 8);
-    public final JoystickButton dLStickBtn = new JoystickButton(driver, 9);
-    public final JoystickButton dRStickBtn = new JoystickButton(driver, 10);
-    public final AxisButton dRTrigger = new AxisButton(driver, 3, .05);
-    public final AxisButton dLTrigger = new AxisButton(driver, 2, .05);
+//    public final JoystickButton dA = new JoystickButton(driver, 1);
+//    public final JoystickButton dB = new JoystickButton(driver, 2);
+//    public final JoystickButton dX = new JoystickButton(driver, 3);
+//    public final JoystickButton dY = new JoystickButton(driver, 4);
+//    public final JoystickButton dLBump = new JoystickButton(driver, 5);
+//    public final JoystickButton dRBump = new JoystickButton(driver, 6);
+//    public final JoystickButton dBack = new JoystickButton(driver, 7);
+//    public final JoystickButton dStart = new JoystickButton(driver, 8);
+//    public final JoystickButton dLStickBtn = new JoystickButton(driver, 9);
+//    public final JoystickButton dRStickBtn = new JoystickButton(driver, 10);
+    public final Trigger dRTrigger = driver.rightTrigger();
 
-    public final POVButton dDPadUp = new POVButton(driver, 0);
-    public final POVButton dDPadRight = new POVButton(driver, 90);
-    public final POVButton dDPadDown = new POVButton(driver, 180);
-    public final POVButton dDPadLeft = new POVButton(driver, 270);
+    //public final Trigger dRTrigger =  new JoystickButton(driver, XboxController.Axis.RightTrigger);
+//    public final AxisButton dLTrigger = new AxisButton(driver, 2, .05);
+//
+//    public final POVButton dDPadUp = new POVButton(driver, 0);
+//    public final POVButton dDPadRight = new POVButton(driver, 90);
+//    public final POVButton dDPadDown = new POVButton(driver, 180);
+//    public final POVButton dDPadLeft = new POVButton(driver, 270);
 
     public final JoystickButton aA = new JoystickButton(aux, 1);
     public final JoystickButton aB = new JoystickButton(aux, 2);
@@ -57,7 +63,7 @@ public class OI {
     public final JoystickButton aStart = new JoystickButton(aux, 8);
     public final JoystickButton aLStickBtn = new JoystickButton(aux, 9);
     public final JoystickButton aRStickBtn = new JoystickButton(aux, 10);
-    public final AxisButton aRTrigger = new AxisButton(aux, 3, .05);
+    //public final Trigger aRTrigger =  new JoystickButton(driver, XboxController.Axis.kRightTrigger.value);
     public final AxisButton aLTrigger = new AxisButton(aux, 2, .05);
     public final POVButton aDPadUp = new POVButton(aux, 0);
     public final POVButton aDPadRight = new POVButton(aux, 90);
@@ -104,7 +110,8 @@ public class OI {
 //        aB.onTrue(new Co)
         aA.onTrue(new InstantCommand(Robot.elevator::zeroEncoder));
 
-        dStart.onTrue(new ConeLEDs());
-        dBack.onTrue(new CubeLEDs());
+//        dStart.onTrue(new ConeLEDs());
+//        dBack.onTrue(new CubeLEDs());
+        dRTrigger.onTrue(new PrintCommand("Can this work jesus christ"));
     }
 }
