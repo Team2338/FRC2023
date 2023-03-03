baa242916485b531b31199c78a7b4d05bba88096
Bill Knopf <billknopfjr@yahoo.com>
2023-03-02 23:33:47 -0600

    Added generic drive auto class

--- src/main/java/team/gif/robot/commands/autos/PlaceCollectPlace.java
+++ src/main/java/team/gif/robot/commands/autos/PlaceCollectPlace.java
@@ -3,5 +3,12 @@ package team.gif.robot.commands.autos;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class PlaceCollectPlace extends SequentialCommandGroup {
-    //Need specific directions depending on side
+    public PlaceCollectPlace(){
+        addCommands(
+                new Drive(1.2).withTimeout(1.5),
+                new Drive(0.3).withTimeout(1.5),
+                new Drive(-0.3).withTimeout(1.5),
+                new Drive(-1.2).withTimeout(1.5)
+        );
+    }
 }
