c3e0f420a2938078c9e8ac1a5c4ac73d93285b09
DS-2 <billknopfjr@yahoo.com>
2023-03-09 17:15:10 -0600

    WIP

--- src/main/java/team/gif/robot/commands/autos/PlaceCubeHighEngage.java
+++ src/main/java/team/gif/robot/commands/autos/PlaceCubeHighEngage.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import team.gif.robot.Constants;
 import team.gif.robot.commands.arm.SetArmPosition;
+import team.gif.robot.commands.autos.lib.UntilBotIsLevel;
 import team.gif.robot.commands.collector.CollectorEject;
 import team.gif.robot.commands.collector.WheelsIn;
 import team.gif.robot.commands.collector.WheelsOut;
@@ -22,7 +23,8 @@ public class PlaceCubeHighEngage extends SequentialCommandGroup {
             new CollectorEject().withTimeout(0.4),
             new GoHomeStageHome(),
 
-            new DriveAndEngage() // Drive to the charging station and engage
+            new DriveAndEngage(), // Drive to the charging station and engage
+            new UntilBotIsLevel().withTimeout(0.1)
 //            new AutoDrive(-Constants.AutoConstants.DRIVE_FAST).withTimeout(2.6), // drive (time and speed based) until we are angled
 //            new WaitCommand(.25),               // given pigeon time to settle
 //            new ParallelDeadlineGroup(
---
+++ src/main/java/team/gif/robot/commands/autos/lib/UntilBotIsLevel.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package team.gif.robot.commands.autos.lib;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import team.gif.robot.Constants;
+import team.gif.robot.Robot;
+
+public class UntilBotIsLevel extends CommandBase {
+
+    private int posAngle;
+    public UntilBotIsLevel() {
+        super();
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        posAngle=0;
+
+        if( Robot.pigeon.getPitch() > 2.0 ){
+            posAngle=2;
+        } else if( Robot.pigeon.getPitch() < -2.0) {
+            posAngle=1;
+        }
+    }
+
+    // Called every time the scheduler runs (~20ms) while the command is scheduled
+    @Override
+    public void execute() {
+        if( posAngle == 2 ){
+            Robot.swervetrain.drive(-0.3,0,0);
+        } else if( posAngle == 1) {
+            Robot.swervetrain.drive(0.3,0,0);
+        }
+    }
+
+    // Return true when the command should end, false if it should continue. Runs every ~20ms.
+    @Override
+    public boolean isFinished() {
+//        System.out.println("checking pitch " + Robot.pigeon.getPitch());
+        // > -X is coming fron alliance station and falling to become level
+        // < X is falling from opponent side
+        return false; // was -14.0
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted){}
+}
