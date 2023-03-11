9b65ff96e1076e61fb6ef41bcde20742f80bfd04
Bill Knopf <billknopfjr@yahoo.com>
2023-03-10 22:02:45 -0600

    Added NoHomeEngage and removed botlevel timeout

--- src/main/java/team/gif/robot/commands/autos/DriveAndEngage.java
+++ src/main/java/team/gif/robot/commands/autos/DriveAndEngage.java
@@ -1,6 +1,7 @@
 package team.gif.robot.commands.autos;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -9,6 +10,7 @@ import team.gif.robot.Robot;
 import team.gif.robot.commands.autos.lib.AutoDrive;
 import team.gif.robot.commands.autos.lib.UntilBotIsFalling;
 import team.gif.robot.commands.autos.lib.UntilBotIsLevel;
+import team.gif.robot.commands.combo.GoHomeStageHome;
 
 public class DriveAndEngage extends SequentialCommandGroup {
     public DriveAndEngage(){
@@ -18,13 +20,14 @@ public class DriveAndEngage extends SequentialCommandGroup {
             new WaitCommand(.25),               // give pigeon time to settle
             new ParallelDeadlineGroup(
                 new UntilBotIsFalling(),               // monitor gyro until level
-                new AutoDrive(-Constants.AutoConstants.DRIVE_SLOW)
+                new AutoDrive(-Constants.AutoConstants.DRIVE_SLOW),
+                new GoHomeStageHome()
             ),
             new AutoDrive(Constants.AutoConstants.DRIVE_SUPER_SLOW).withTimeout(.35), // give the bot a little push back to stop momentum
             new WaitCommand(0.5),
 
-            new UntilBotIsLevel().withTimeout(3.0),
-            new InstantCommand(()-> Robot.swervetrain.drive(0,0,0.000001))
+            new UntilBotIsLevel(),
+            new InstantCommand(()-> Robot.swervetrain.drive(0,0,0.0001))
         );
     }
 }
