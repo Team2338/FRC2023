9b477a2bc3ffd8baa28fe6ec032d688048c07920
Bill Knopf <billknopfjr@yahoo.com>
2023-03-27 20:26:48 -0500

    lower elevator stage, limit new drive sped, fix autoalign

--- src/main/java/team/gif/robot/commands/drivetrain/DriveSwerve.java
+++ src/main/java/team/gif/robot/commands/drivetrain/DriveSwerve.java
@@ -25,6 +25,9 @@ public class DriveSwerve extends CommandBase {
     @Override
     public void execute() {
         if (Robot.isCompBot) {
+            double forwardSign;
+            double strafeSign;
+
             double forward = -Robot.oi.driver.getLeftY(); // need to invert because -Y is away, +Y is pull back
             forward = (Math.abs(forward) > Constants.Joystick.DEADBAND) ? forward : 0.0; //0.00001;
 
@@ -34,8 +37,13 @@ public class DriveSwerve extends CommandBase {
             double rot = -Robot.oi.driver.getRightX(); // need to invert because left is negative, right is positive
             rot = (Math.abs(rot) > Constants.Joystick.DEADBAND) ? rot : 0.0;
 
+            forwardSign = forward/Math.abs(forward);
+            strafeSign = strafe/Math.abs(strafe);
             // Use a parabolic curve (instead if linear) for the joystick to speed ratio
             // This allows for small joystick inputs to use slower speeds
+
+            forward = forward * Math.abs(forward);
+            strafe = strafe * Math.abs(strafe);
 
             forward = .5 * Math.sqrt(2 + forward*forward - strafe*strafe + 2*forward*Math.sqrt(2)) -
                     .5 * Math.sqrt(2 + forward*forward - strafe*strafe - 2*forward*Math.sqrt(2));
@@ -43,8 +51,10 @@ public class DriveSwerve extends CommandBase {
             strafe = .5 * Math.sqrt(2 - forward*forward + strafe*strafe + 2*strafe*Math.sqrt(2)) -
                     .5 * Math.sqrt(2 - forward*forward + strafe*strafe - 2*strafe*Math.sqrt(2));
 
-//            forward = forward * Math.abs(forward);
-//            strafe = strafe * Math.abs(strafe);
+            if( Double.isNaN(forward) )
+                forward = forwardSign;
+            if( Double.isNaN(strafe) )
+                strafe = strafeSign;
 
             //Forward speed, Sideways speed, Rotation Speed
             forward = forwardLimiter.calculate(forward) * Constants.ModuleConstants.TELE_DRIVE_MAX_SPEED_METERS_PER_SECOND;
